33fb0c843f43f65cf7334a395bc8f8a1642d296a
briannaordaz <briannaordaz5@gmail.com>
2025-02-22 00:08:00 -0800

    added author, course, faculty, and purpose

---
+++ COMP003A.ZooManagementSystem/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace COMP003A.ZooManagementSystem;
+
+// Author: Brianna Ordaz
+// Course: COMP-003A
+// Faculty:Jonathan Cruz
+// Purpose: Zoo management system demonstrating inheritance, abstraction, polymorphism, and method overloading in C#
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
